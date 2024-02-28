2ac09a0abf7cdf8b076518d9d5c2d790280abefc
pereztamo <159158002+pereztamo@users.noreply.github.com>
2024-02-29 00:41:17 +0100

    Update PlayerShot.cs

--- Galaga/PlayerShot.cs
+++ Galaga/PlayerShot.cs
@@ -6,7 +6,7 @@ namespace Galaga;
 public class PlayerShot {
     private static Vec2F extent = new Vec2F(0.008f, 0.021f);
     private static Vec2F direction = new Vec2F(0.0f, 0.1f);
-    // det her er opgave 6.4.1
+    // det her er opgave 6.4.1. Husk at inherit fra "Entity", da jeg ikke få lov at commit uden den viser fejl
     // er kun lige startet og du fortsætter bare. alt andet builder fint men hvis du builder får du fejl da den her er ikke færdigt endnu
 
     public PlayerShot(Vec2F position, IBaseImage image){
