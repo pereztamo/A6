5e2a89759d1ada07c8c36e481158f8b65c803ebc
pereztamo <159158002+pereztamo@users.noreply.github.com>
2024-02-29 00:36:04 +0100

    Update PlayerShot.cs

--- Galaga/PlayerShot.cs
+++ Galaga/PlayerShot.cs
@@ -3,7 +3,7 @@ using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 
 namespace Galaga;
-public class PlayerShot :Entity {
+public class PlayerShot : Entity {
     private static Vec2F extent = new Vec2F(0.008f, 0.021f);
     private static Vec2F direction = new Vec2F(0.0f, 0.1f);
     // det her er opgave 6.4.1
