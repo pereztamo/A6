bc1ecb11bca2257c1fae8c57a475918bbdec3c8f
pereztamo <159158002+pereztamo@users.noreply.github.com>
2024-02-29 00:22:04 +0100

    Update Game.cs

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,34 +1,89 @@
+using System.IO;
 using DIKUArcade;
 using DIKUArcade.GUI;
+using DIKUArcade.Math;
 using DIKUArcade.Input;
 using DIKUArcade.Events;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using System.Collections.Generic;
 
 namespace Galaga;
 public class Game : DIKUGame, IGameEventProcessor {
+    private Player player;
+    private GameEventBus eventBus;
+    private EntityContainer<Enemy> enemies;
     public Game(WindowArgs windowArgs) : base(windowArgs) {
+        player = new Player(
+            new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+            new Image(Path.Combine("Assets", "Images", "Player.png")));
+        eventBus = new GameEventBus();
+        eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent });
+        window.SetKeyEventHandler(KeyHandler);
+        eventBus.Subscribe(GameEventType.InputEvent, this);
+            List<Image> images = ImageStride.CreateStrides (
+                4, Path.Combine("Assets", "Images", "BlueMonster.png"));
+            const int numEnemies = 8; 
+            enemies = new EntityContainer<Enemy>(numEnemies); 
+            for (int i = 0; i < numEnemies; i++) {
+                enemies.AddEntity(new Enemy(
+                    new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                    new ImageStride(80, images)));
+            }
+        
         // TODO: Set key event handler (inherited window field of DIKUGame class)
 
     }
 
 
-    public override void Render(){
-        throw new System.NotImplementedException("Galaga game has nothing to render yet.");
+    public override void Render() {
+        player.Render();
+        enemies.RenderEntities();
     }
 
-    public override void Update(){
-        throw new System.NotImplementedException("Galaga game has no entities to update yet.");
+    public override void Update() {
+        eventBus.ProcessEventsSequentially();
+        player.Move();
     }
 
     private void KeyPress(KeyboardKey key) {
+        switch (key) {
+            case KeyboardKey.Escape:
+                window.CloseWindow();
+                break;
+            case KeyboardKey.Left:
+                player.SetMoveLeft(true);
+                break;
+            case KeyboardKey.Right:
+                player.SetMoveRight(true);
+                break;
+        }
         // TODO: Close window if escape is pressed
         // TODO: switch on key string and set the player's move direction
     }
 
     private void KeyRelease(KeyboardKey key) {
+        switch (key) {
+            case KeyboardKey.Left:
+                player.SetMoveLeft(false);
+                break;
+            case KeyboardKey.Right:
+                player.SetMoveRight(false);
+                break;
+        }
         // TODO: switch on key string and disable the player's move direction
     }
 
     private void KeyHandler(KeyboardAction action, KeyboardKey key) {
+        switch (action) {
+            case KeyboardAction.KeyRelease:
+                KeyRelease(key);
+                break;
+            case KeyboardAction.KeyPress:
+                KeyPress(key);
+                break;
+        }
+
         // TODO: Switch on KeyBoardAction and call proper method
         }
 
