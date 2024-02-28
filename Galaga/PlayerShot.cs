f13e883df5f278a88d9a771f48155faa2e8b524b
pereztamo <159158002+pereztamo@users.noreply.github.com>
2024-02-29 00:28:19 +0100

    Update PlayerShot.cs

--- Galaga/PlayerShot.cs
+++ Galaga/PlayerShot.cs
@@ -3,7 +3,11 @@ using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 
 namespace Galaga;
-public class PlayerShot {
+public class PlayerShot : Entity {
+    private static Vec2F extent = new Vec2F(0.008f, 0.021f);
+    private static Vec2F direction = new Vec2F(0.0f, 0.1f);
+    // det her er opgave 6.4.1
+    // er kun lige startet og du fortsætter bare. alt andet builder fint men hvis du builder får du fejl da den her er ikke færdigt endnu
 
     public PlayerShot(Vec2F position, IBaseImage image){
 
