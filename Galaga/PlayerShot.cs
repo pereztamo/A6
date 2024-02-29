fc7b8cce0350601552ab1529298de8a3747f69d3
teod0036 <73177141+teod0036@users.noreply.github.com>
2024-02-29 17:36:58 +0100

    Overrode Move()-method in PlayerShot.cs

--- Galaga/PlayerShot.cs
+++ Galaga/PlayerShot.cs
@@ -3,13 +3,15 @@ using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 
 namespace Galaga;
-public class PlayerShot {
+public class PlayerShot : Entity {
     private static Vec2F extent = new Vec2F(0.008f, 0.021f);
     private static Vec2F direction = new Vec2F(0.0f, 0.1f);
-    // det her er opgave 6.4.1. Husk at inherit fra "Entity", da jeg ikke få lov at commit uden den viser fejl
-    // er kun lige startet og du fortsætter bare. alt andet builder fint men hvis du builder får du fejl da den her er ikke færdigt endnu
 
-    public PlayerShot(Vec2F position, IBaseImage image){
+    public PlayerShot(Vec2F position, IBaseImage image) : base(new DynamicShape(position, extent, direction), image){
 
+    }
+
+    public override void Move() {
+        base.Move(direction);
     }
 }
