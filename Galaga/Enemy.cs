92ebd2667e2181b0e02701bbe8b89a62b70ef311
teod0036 <73177141+teod0036@users.noreply.github.com>
2024-03-02 22:37:11 +0100

    Added Render() to Enemy.cd and PlayerShot.cs

--- Galaga/Enemy.cs
+++ Galaga/Enemy.cs
@@ -7,4 +7,9 @@ public class Enemy : Entity{
     public Enemy(DynamicShape shape, IBaseImage image) : base(shape, image) {
     }
 
+    //According to the deliverable this method should exist, 
+    //but was never mentioned in the actual assignment text so it just exists and is unused
+    public void Render() {
+        RenderEntity();
+    }
 }
